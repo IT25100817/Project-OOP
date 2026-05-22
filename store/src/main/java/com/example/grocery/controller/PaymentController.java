6bf1cd6182eaa0971b4d29d90ffd782cd7fb358e
IT25103048 <IT25103048@my.sliit.lk>
2026-05-23 00:31:39 +0530

    IT25103048 Add PaymentController endpoints

--- store/src/main/java/com/example/grocery/controller/PaymentController.java
+++ store/src/main/java/com/example/grocery/controller/PaymentController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+
 @RequestMapping("/payments")
 public class PaymentController {
     private final PaymentService paymentService;
